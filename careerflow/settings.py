4440b6d6f7772faba13031e41734c2741eb05bab
Umadevibalakrishnan <umadevibalakrishnan39@gmail.com>
2026-03-02 22:34:06 +0530

    updated settings for deployment

--- careerflow/settings.py
+++ careerflow/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+*(&2^z*pp@y-csn()7ukkf+gk+p^u_r!^ei1w!5v#5#pia&^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
